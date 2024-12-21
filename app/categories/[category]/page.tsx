9c886a4464c8999f2aebeac8337e5c72da7f4986
Israel Rotimi <130306892+israelrotimi@users.noreply.github.com>
2024-12-21 07:39:39 +0100

    Update page.tsx

--- app/categories/[category]/page.tsx
+++ app/categories/[category]/page.tsx
@@ -57,4 +57,3 @@ export const getServerSideProps: GetServerSideProps<PageProps> = async (context)
                                   },
                                     };
                                     };
-}
